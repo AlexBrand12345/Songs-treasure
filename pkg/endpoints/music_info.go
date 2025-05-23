3ecff857529682f993613a32ea3049bedd29d8a4
AlexBrand <market201483@gmail.com>
2025-05-23 13:54:48 +0300

    example update

--- pkg/endpoints/music_info.go
+++ pkg/endpoints/music_info.go
@@ -18,7 +18,9 @@ func GetMusicsInfo(group, song string) (releaseDate, text, link string, err erro
 	var url string
 	var responseStruct MusicInfoResponse
 
-	// return "22.10.2020", "blabla\nstrange\n\ntest\n\nanotherone\nend", "", nil
+	if config.MUSIC_INFO_URL == "" {
+		return "22.10.2020", "blabla\nstrange\n\ntest\n\nanotherone\nend", "", nil
+	}
 
 	url = config.MUSIC_INFO_URL + "/info" + fmt.Sprintf("?group=%s&song=%s", group, song)
 
